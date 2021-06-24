d79986356dc52cf61e4c1ed7acdacdcf43616e09
tieje <toj320@gmail.com>
2021-06-24 10:47:46 -0400

    console is working

--- console.py
+++ console.py
@@ -9,12 +9,22 @@ class HBNBCommand(cmd.Cmd):
     """
     The entry point of the command interpreter
     """
-    prompt = '(hbnb)'
+    prompt = '(hbnb) '
 
-    def __init__(self, completekey='tab', stdin=None, stdout=None):
-        super().__init__()
-        if self.stdin == 'quit' or self.stdin == 'EOF':
-            quit()
+    def do_quit(self, arg):
+        """quit command quits console"""
+        quit()
+
+    def do_EOF(self, arg):
+        """EOF command quits the console"""
+        quit()
+
+    def emptyline(self):
+        """
+        Override emptyline() to return nothing
+        instead of the last line
+        """
+        return
 
 
 if __name__ == '__main__':
