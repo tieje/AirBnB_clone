a02b05a19a11e63fcbfe2a065801c973f7d96446
tieje <toj320@gmail.com>
2021-06-24 09:38:22 -0400

    added top of the line of each file

--- console.py
+++ console.py
@@ -1,16 +1,19 @@
+#!/usr/bin/python3
 """
 Entry point of the command interpreter
 """
 import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     """
     The entry point of the command interpreter
     """
     prompt = '(hbnb)'
-    def __init__(self):
+
+    def __init__(self, completekey='tab', stdin=None, stdout=None):
         super().__init__()
-        if self.stdin == 'quit':
+        if self.stdin == 'quit' or self.stdin == 'EOF':
             quit()
 
 
