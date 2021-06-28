0f7117f483f6d833580f99f69144a69538118f93
Nels <nelsfichera@gmail.com>
2021-06-28 13:56:41 -0400

    add more tests

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Contain tests for the state module. '''
+import unittest
+from models.base_model import BaseModel
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    '''Test the State class.'''
+
+    def test_State_inheritence(self):
+        '''Test that state class inherits'''
+        new_state = State()
+        self.assertIsInstance(new_state, BaseModel)
+
+    def test_State_attributes(self):
+        '''Test that state has name'''
+        new_state = State()
+        self.assertTrue("name" in new_state.__dir__())
+
+    def test_State_attributes_type(self):
+        '''Test state class attribute name str'''
+        new_state = State()
+        name = getattr(new_state, "name")
+        self.assertIsInstance(name, str)
