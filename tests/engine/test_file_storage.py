290c809f04fb8cb29fdb0d8fb56ba6fbb4562cbf
Nels <nelsfichera@gmail.com>
2021-06-29 08:08:43 -0400

    Move to engine folder

---
+++ tests/engine/test_file_storage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''Test file storage file'''
+import unittest
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+
+class TestBase(unittest.TestCase):
+    '''test file_storage.py file'''
+
+    def setUp(self):
+        '''Initializing'''
+        self.storage = FileStorage()
+
+    def TearDown(self):
+        '''Removing'''
+        del self.storage
+
+    def test_all(self):
+        '''test all method'''
+        self.assertEqual(type(self.storage.all()), dict)
+
+    def test_reload(self):
+        '''test reload method'''
+        self.storage.reload()
+        self.assertEqual(type(self.storage.all()), dict)
+
+    def test_new(self):
+        '''test new method'''
+        self.storage.reload()
+        new_model = BaseModel()
+        self.assertEqual(type(new_model.id), str)
+
+    def test_save(self):
+        '''test save method'''
+        self.storage.reload()
+        new_model = BaseModel()
+        new_model.save()
+        new_model_dict = new_model.to_dict()
+        data = self.storage.all()['BaseModel.' + new_model_dict['id']]
+        self.assertEqual(new_model_dict['id'], data.id)
